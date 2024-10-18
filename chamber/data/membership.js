103d851f557e88b34916b713a37e91bee331d7a8
sullaymansamai3@gmail.com <sullaymansamai3@gmail.com>
2024-10-18 15:25:28 +0000

    i am done with the discover page

---
+++ chamber/data/membership.js
@@ -0,0 +1,26 @@
+export const membershipLevels =  [
+    {
+        "level": "Gold",
+        "id": "gold",
+        "price": "$500 per year",
+        "benefits": ["All the benefits of the lower tiers plus...", "Advertising spotlight on chamber home page", "4 business mentoring meetings per year"]
+    },
+    {
+        "level": "Silver",
+        "id": "silver",
+        "price": "$300 per year",
+        "benefits": ["All the benefits of the lower tiers plus...", "Use of our \"Resources and Education for Businesses\" Center"]
+    },
+    {
+        "level": "Bronze",
+        "id": "bronze",
+        "price": "$200 per year",
+        "benefits": ["All the benefits of the non-profit membership plus...", "Business marketing workshops", "Invitations to 60+ networking events"]
+    },
+    {
+        "level": "Non-profit",
+        "id": "np",
+        "price": "Free to non-profit organizations",
+        "benefits": ["Exclusive Chamber Newsletter access", "Increased visability via our public chamber membership directory"]
+    }
+]
